e90431742d5cc4475f57b470ff64a7210e86c872
Gerasim Troeglazov <3dEyes@gmail.com>
2015-07-19 22:32:42 +1000

    Add sliders for quality, noise, fps and threads params

--- includes/PanoramaSaver.h
+++ includes/PanoramaSaver.h
@@ -2,10 +2,19 @@
 #include <StringView.h>
 #include <String.h>
 #include <Slider.h>
+#include <TextView.h>
 #include <Font.h>
 #include <CheckBox.h>
 
 #include "FBView.h"
+
+// message constants
+enum {
+	MSG_SET_FPS_LIMIT		= 'fpsl',
+	MSG_SET_CPU_LIMIT		= 'cpul',
+	MSG_SET_QUALITY			= 'qual',
+	MSG_SET_NOISE_LEVEL		= 'nois',
+};
 
 class PanoramaSaver : public BScreenSaver, public BHandler 
 {
@@ -19,7 +28,6 @@ public:
 	virtual	status_t	SaveState(BMessage* into) const;
 	virtual	void		MessageReceived(BMessage* message);
 private:
-	BStringView		*tView;
 	FBView			*frameBuffer;
 	thread_id 		rendererThread;
 };
--- src/PanoramaSaver.cpp
+++ src/PanoramaSaver.cpp
@@ -1,5 +1,9 @@
+#include <ControlLook.h>
+#include <InterfaceKit.h>
+#include <LayoutBuilder.h>
 #include <StringView.h>
 #include <TranslationUtils.h>
+#include <TranslatorFormats.h>
 #include <ScreenSaver.h>
 #include <Slider.h>
 #include <stdlib.h>
@@ -18,6 +22,134 @@
 #include "BlurFilter.h"
 #include "NoiseFilter.h"
 
+int32			fFPSLimit;
+int32			fCPULimit;
+int32			fQuality;
+int32			fNoiseLevel;
+BPath			fFilename;
+PRender 		*fRender;
+
+class SimpleSlider : public BSlider {
+public:
+	SimpleSlider(const char* label, int32 min, int32 max, BMessage* message)
+		:
+		BSlider(B_EMPTY_STRING, B_EMPTY_STRING, message, min, max, B_HORIZONTAL)
+	{
+		BString minLabel;
+		minLabel << min;
+		BString maxLabel;
+		maxLabel << max;
+		SetLimitLabels(minLabel, maxLabel);
+		SetHashMarks(B_HASH_MARKS_BOTTOM);
+		SetHashMarkCount(11);
+		fLabel = label;
+	};
+
+	const char* UpdateText() const
+	{
+		fText.SetToFormat("%s: %d", fLabel, Value());
+		return fText.String();
+	};
+
+private:
+	mutable BString fText;
+	const char* fLabel;
+};
+
+class SettingsView : public BView {
+public:
+								SettingsView(BRect frame);
+
+	virtual	void				AttachedToWindow();
+	virtual	void				MessageReceived(BMessage* message);
+
+	private:
+			BSlider*			fFPSSlider;
+			BSlider*			fCPUSlider;
+			BSlider*			fQualitySlider;
+			BSlider*			fNoiseSlider;
+};
+
+
+SettingsView::SettingsView(BRect frame)
+	:
+	BView(frame, "", B_FOLLOW_ALL, B_WILL_DRAW)
+{
+	BStringView* titleString = new BStringView(B_EMPTY_STRING, "Hanarama 360");
+	titleString->SetFont(be_bold_font);
+
+	BStringView* copyrightString = new BStringView(B_EMPTY_STRING,
+		"© 2013-2015 Gerasim Troeglazov");
+
+	fFPSSlider = new SimpleSlider("FPS Limit", 1, 120, new BMessage(MSG_SET_FPS_LIMIT));
+	fFPSSlider->SetValue(fFPSLimit);
+
+	fCPUSlider = new SimpleSlider("Threads", 1, 32,	new BMessage(MSG_SET_CPU_LIMIT));
+	fCPUSlider->SetValue(fCPULimit);
+
+	fQualitySlider = new SimpleSlider("Quality", 10, 100, new BMessage(MSG_SET_QUALITY));
+	fQualitySlider->SetValue(fQuality);
+
+	fNoiseSlider = new SimpleSlider("Noise", 0, 100, new BMessage(MSG_SET_NOISE_LEVEL));
+	fNoiseSlider->SetValue(fNoiseLevel);
+
+
+	BLayoutBuilder::Group<>(this, B_VERTICAL, B_USE_HALF_ITEM_SPACING)
+		.SetInsets(B_USE_HALF_ITEM_INSETS, B_USE_HALF_ITEM_INSETS,
+			B_USE_BIG_INSETS, B_USE_HALF_ITEM_INSETS)
+		.Add(titleString)
+		.Add(copyrightString)
+		.AddStrut(roundf(be_control_look->DefaultItemSpacing() / 2))
+		.AddGlue()
+		.Add(fFPSSlider)
+		.AddGlue()
+		.Add(fCPUSlider)
+		.AddGlue()
+		.Add(fQualitySlider)
+		.AddGlue()
+		.Add(fNoiseSlider)
+		.AddGlue()
+	.End();
+
+	MoveBy(0, -25); // The view is not where it should be.
+}
+
+
+void
+SettingsView::AttachedToWindow()
+{
+	SetViewColor(ui_color(B_PANEL_BACKGROUND_COLOR));
+	fFPSSlider->SetTarget(this);
+	fCPUSlider->SetTarget(this);
+	fQualitySlider->SetTarget(this);
+	fNoiseSlider->SetTarget(this);
+}
+
+
+void
+SettingsView::MessageReceived(BMessage* message)
+{
+	switch(message->what) {
+		case MSG_SET_FPS_LIMIT:
+			fFPSLimit = fFPSSlider->Value();
+			break;
+
+		case MSG_SET_CPU_LIMIT:
+			fCPULimit = fCPUSlider->Value();
+			break;
+
+		case MSG_SET_QUALITY:
+			fQuality = fQualitySlider->Value();
+			break;
+			
+		case MSG_SET_NOISE_LEVEL:
+			fNoiseLevel = fNoiseSlider->Value();
+			break;			
+	}
+}
+
+
+
 namespace BPrivate {
 	void BuildScreenSaverDefaultSettingsView(BView* view, const char* moduleName, const char* info);
 }
@@ -30,8 +162,25 @@ extern "C" _EXPORT BScreenSaver *instantiate_screen_saver(BMessage *message, ima
 }
 
 PanoramaSaver::PanoramaSaver(BMessage *message, image_id image)
- : BScreenSaver(message, image), BHandler("HanaramaSaver")
+	:
+	BScreenSaver(message, image),
+ 	BHandler("HanaramaSaver")
 {
+	if (message) {
+		if (message->FindInt32("FPS Limit", &fFPSLimit) != B_OK)
+			fFPSLimit = 30;
+		if (message->FindInt32("CPU Limit", &fCPULimit) != B_OK)
+			fCPULimit = 1;
+		if (message->FindInt32("Quality", &fQuality) != B_OK)
+			fQuality = 100;
+		if (message->FindInt32("Noise level", &fNoiseLevel) != B_OK)
+			fNoiseLevel = 0;
+		const char *filename;
+		if (message->FindString("Filename", &filename) != B_OK)
+			fFilename.Unset();
+		else
+			fFilename.SetTo(filename);
+	}
 }
 
 PanoramaSaver::~PanoramaSaver()
@@ -43,15 +192,23 @@ PanoramaSaver::~PanoramaSaver()
 
 void PanoramaSaver::StartConfig(BView *view)
 {
-	BPrivate::BuildScreenSaverDefaultSettingsView(view, "Hanarama 360",
-		"(c) 2013-2015 Gerasim Troeglazov");
+	view->AddChild(new SettingsView(view->Frame()));
 }
 
 
 status_t
 PanoramaSaver::SaveState(BMessage* into) const
 {
-	return B_BAD_VALUE;
+	status_t status;
+	if ((status = into->AddInt32("FPS Limit", fFPSLimit)) != B_OK)
+		return status;
+	if ((status = into->AddInt32("CPU Limit", fCPULimit)) != B_OK)
+		return status;
+	if ((status = into->AddInt32("Quality", fQuality)) != B_OK)
+		return status;
+	if ((status = into->AddInt32("Noise level", fNoiseLevel)) != B_OK)
+		return status;
+	return B_OK;
 }
 
 
@@ -65,8 +222,6 @@ float getTime(void)
 	return system_time()/1000000.0;
 }
 
-bool DIE = false;
-
 int32 renderer(void *data)
 {
 	float delay = 0;
@@ -78,35 +233,40 @@ int32 renderer(void *data)
 	
   	BBitmap *dstBmp = view->GetBitmap();
   	BBitmap *srcBmp;
-	if(view->fPreview)
-		srcBmp = BTranslationUtils::GetBitmapFile("/boot/home/Project/Hanarama/samples/small.jpg");
-	else
-		srcBmp = BTranslationUtils::GetBitmapFile("/boot/home/Project/Hanarama/samples/test.jpg");
+	//if(view->fPreview)
+		//srcBmp = BTranslationUtils::GetBitmap(B_JPEG_FORMAT, "default.jpg");
+		 srcBmp = BTranslationUtils::GetBitmapFile("/HaikuData/Projects/Hanarama/samples/test.jpg");
+	//else
+		//srcBmp = BTranslationUtils::GetBitmapFile("/HaikuData/Projects/Hanarama/samples/test.jpg");
 
-	PRender *render = new PRender(srcBmp, dstBmp, fCam);
+	fRender = new PRender(srcBmp, dstBmp, fCam);
 	
 	PFadeFilter fader(dstBmp);
 	PBlurFilter blurer(dstBmp);
 	PNoiseFilter noise(dstBmp);
 		
 	fader.SetFade(255);
-  	noise.SetDispersion(50);
+
+	int lastDispersion = fNoiseLevel;
+  	noise.SetDispersion(fNoiseLevel);
   	
-	render->InitMultiRenders(2);
+	fRender->InitMultiRenders(fCPULimit);
 		
 	float time;
-	float fps_limit = 30.0;
 
 	_starttime = _lasttime = getTime();
 
-  	for(;!DIE;counter++) {
-		if(!DIE)
-			render->Render();
-		if(!DIE)
-			fader.Apply();
-    	//noise.Apply();
-		if(!DIE)
-			view->Paint();
+  	for(;;counter++) {
+		fRender->Render();
+		fader.Apply();
+    	if(fNoiseLevel>0) {
+    		if(fNoiseLevel != lastDispersion) {
+    			noise.SetDispersion(fNoiseLevel);
+    			lastDispersion = fNoiseLevel;
+    		}
+    		noise.Apply();
+    	}
+		view->Paint();
 
 		time = getTime();
 		if(time - _lasttime > 1.0) {
@@ -115,12 +275,12 @@ int32 renderer(void *data)
 			int fps_int = (int)fps;
 			if(fps_int<0)fps_int=0;
 
-			float znooze_frame = ((1/fps_limit) - (1/fps)) * 1000000;		
+			float znooze_frame = ((1/(float)fFPSLimit) - ((1/fps) - (delay/1000000))) * 1000000;			
 
 			if(znooze_frame > 0)
 				delay = znooze_frame;
 
-			//sprintf(temp,"FPS: %d %d",(int)fps_int, (int)delay);
+			//sprintf(temp,"FPS: %d",(int)fps_int);
 			//view->SetOSD(temp);			
 			counter = -1;
 		}
@@ -136,7 +296,7 @@ int32 renderer(void *data)
 		if(delay>0 && delay < 1000000)
 			snooze((int)delay);
   	}
-  	render->LeaveMultiRender();  	
+  	fRender->LeaveMultiRender();
   	return 0;
 }
 
@@ -144,21 +304,20 @@ void PanoramaSaver::StopSaver(void)
 {
 	status_t ret = 0;
 	fCam->stopThread();
-	DIE = true;
-	wait_for_thread(rendererThread, &ret);
+	fRender->LeaveMultiRender();
+	kill_thread(rendererThread);
 }
 
 status_t 
 PanoramaSaver::StartSaver(BView *view, bool preview)
 {	
-	DIE = false;
 	fCam = new PCamera();
 	fCam->SetMode(CAM_MODE_AUTO_PANNIG);
 	
 	if(preview) {
 		frameBuffer = new FBView(view->Bounds(), view->Bounds().Width(), view->Bounds().Height(), preview);
 	} else {
-		frameBuffer = new FBView(view->Bounds(), (view->Bounds().Width() + 1) / 2, (view->Bounds().Height() + 1) / 2, preview);
+		frameBuffer = new FBView(view->Bounds(), view->Bounds().Width() * fQuality / 100.0 , view->Bounds().Height()* fQuality / 100.0, preview);
 	}
 	view->AddChild(frameBuffer);
 	
